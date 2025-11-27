2907b04e695ea3cf7028e643ec5453c85f898551
sabilahsyaharani19-byte <sabilahsyaharani19@gmail.com>
2025-11-27 16:09:15 +0700

    Add files via upload

---
+++ pertemuan-ddp-2/haha.py
@@ -0,0 +1,12 @@
+
+# Program ini seharusnya menampilkan profil singkat seorang mahasiswa.
+
+NAMA_MAHASISWA = Budi Perkasa
+nim = "123456789"
+angkatan = 2024
+
+print(--- Profil Mahasiswa ---)
+print("Nama:", nama_mahasiswa)
+print("NIM:", NIM)
+print("Angkatan:", angkatan)
+print("Selamat datang di dunia koding!")
---
+++ pertemuan-ddp-2/yuk.py
@@ -0,0 +1,9 @@
+# Program ini seharusnya menampilkan profil singkat seorang mahasiswa
+nama_mahasiswa ="budi perkasa"
+nim = "123456789"
+angkatan = 2024
+print("--- Profil Mahasiswa ---")
+print("nama:", nama_mahasiswa)
+print("nim:", nim)
+print("Angkatan", angkatan)
+print("selamata datang di dunia koding!")
