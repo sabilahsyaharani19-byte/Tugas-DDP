8df4fd501da10a3fe06aecb8e75618899883bd51
sabilahsyaharani19-byte <sabilahsyaharani19@gmail.com>
2025-11-27 16:06:03 +0700

    Add files via upload

---
+++ pertemuan-ddp-10/panggil.py
@@ -0,0 +1,19 @@
+import bangun_datar, bangun_ruang
+
+print("====Luas Bangun Datar====")
+print(f"Luas Persegi: {bangun_datar.luas_persegi(5)}")
+print(f"Luas Segitiga: {bangun_datar.luas_segitiga(4, 6)}")
+print(f"Luas Lingkaran: {bangun_datar.luas_lingkaran(7)}")
+print(f"Luas Ketupat: {bangun_datar.luas_ketupat(6, 8)}")
+print(f"Luas Jajar Genjang: {bangun_datar.luas_jajar_genjang(5, 10)}")
+
+print("====Luas Bangun Ruang====")
+print(f"Luas Kubus: {bangun_ruang.luas_kubus(4)}")
+print(f"Luas Balok: {bangun_ruang.luas_balok(5, 3, 4)}")
+print(f"Luas Bola: {bangun_ruang.luas_bola(6)}")
+print(f"Luas Tabung: {bangun_ruang.luas_tabung(3, 7)}")
+print(f"Luas Kerucut: {bangun_ruang.luas_kerucut(4, 5)}")
+
+
+
+    
